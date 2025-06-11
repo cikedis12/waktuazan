2c677e43d0f3e782724bd0cb3f4ddb53aec26dfd
cikedis12 <muzammilmunif79@gmail.com>
2025-06-11 17:23:30 +0800

    Update and rename app.py to config.py

---
+++ test/config.py
@@ -0,0 +1,12 @@
+# Dahua camera configuration
+CAMERA_SNAPSHOT_URL = "http://10.254.170.163/cgi-bin/snapshot.cgi"
+CAMERA_USERNAME = "admin"
+CAMERA_PASSWORD = "Coinlabit17"
+
+# Email directory to watch (hMailServer path)
+EMAIL_WATCH_DIR = "C:\\Program Files (x86)\\hMailServer\\Data\\localmail.test\\notify"
+
+# Paths
+KNOWN_FACES_DIR = "known_faces"
+SNAPSHOT_SAVE_PATH = "snapshots/latest.jpg"
+TEMPLATE_PATH = "templates/welcome.html"
